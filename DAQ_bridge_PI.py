096c21ea2bb2879de46b00445febe4026c3c6d9d
mahoep <mhoeper3234@gmail.com>
2018-01-06 08:18:42 +0000

    Stuff

--- DAQ_bridge_PI.py
+++ DAQ_bridge_PI.py
@@ -8,25 +8,27 @@ for ports in p:
 # is connected to.
 
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+ser.reset_input_buffer()
+ser.reset_output_buffer()
 print("connected to: " + ser.portstr)
 t = 0
 filename = 'dataFile.csv'
 
-with open(filename, 'w', newline='') as fp:
+with open(filename, 'w',newline='', errors='replace') as fp:
     time.sleep(1)
     tic = time.time()
     print("writing to", filename)
     while (1):
         t = t + 1
-        # reads the incoming data and converts it from b'xxx'r\n\ to a str
+        # reads the incoming data and converts it from b'xxx'r\n\ to a str        
         line = ser.readline()
-        data = line.strip().decode().split(",")
+        data = line.strip().decode(errors='ignore').strip(",")
         now = time.time() - tic
-
         # write to file=======================
-        csvRow = [now,data[0],data[1]]
-        wr = csv.writer(fp)
-        wr.writerow(csvRow)
+        if t > 5:
+            csvRow = [now,data[0],data[1]]
+            wr = csv.writer(fp)
+            wr.writerow(csvRow)
 
         if now > 3:
             break
