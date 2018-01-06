a3607983ff6727f0f0230aaf29a595a88f1428bb
mahoep <mahoep@users.noreply.github.com>
2018-01-06 00:29:07 -0800

    Matched windows file with unix file

--- DAQ_bridge.py
+++ DAQ_bridge.py
@@ -1,4 +1,9 @@
 import serial, time, csv
+from serial.tools import list_ports
+
+p = list_ports.comports()
+for ports in p:
+    print(ports)
 # establish connection to the serial port that your arduino
 # is connected to.
 ser = serial.Serial('COM3', 9600, timeout=1)
@@ -7,6 +12,8 @@ t = 0
 filename = 'dataFile.csv'
 
 with open(filename, 'w', newline='') as fp:
+    ser.reset_input_buffer()
+    ser.reset_output_buffer()
     time.sleep(1)
     tic = time.time()
     print("writing to", filename)
@@ -14,17 +21,15 @@ with open(filename, 'w', newline='') as fp:
         t = t + 1
         # reads the incoming data and converts it from b'xxx'r\n\ to a str
         line = ser.readline()
-        data = line.strip().decode().split(",")
+        data = line.strip().decode(errors='ignore').split(",")
         now = time.time() - tic
-
         # write to file=======================
-        csvRow = [now,data[0],data[1]]
-        wr = csv.writer(fp)
-        wr.writerow(csvRow)
-
+        if t > 5:
+            csvRow = [now,data[0],data[1]]
+            wr = csv.writer(fp)
+            wr.writerow(csvRow)
         if now > 3:
             break
 toc = time.time() - tic
 print("Data collection complete.",toc,"seconds elapsed.")
 ser.close()
-
