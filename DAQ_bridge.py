e078592d6e5ef85a3cd5aff2f03fde8a40dde7ea
mahoep <mahoep@users.noreply.github.com>
2018-01-05 16:28:29 -0800

    Added more output data

--- DAQ_bridge.py
+++ DAQ_bridge.py
@@ -7,20 +7,22 @@ t = 0
 filename = 'dataFile.csv'
 
 with open(filename, 'w', newline='') as fp:
+    time.sleep(1)
     tic = time.time()
     print("writing to", filename)
     while (1):
         t = t + 1
         # reads the incoming data and converts it from b'xxx'r\n\ to a str
         line = ser.readline()
-        data = line.strip().decode()
+        data = line.strip().decode().split(",")
         now = time.time() - tic
 
         # write to file=======================
-        csvRow = [now, data]
+        csvRow = [now,data[0],data[1]]
         wr = csv.writer(fp)
         wr.writerow(csvRow)
-        if now > 10:
+
+        if now > 3:
             break
 toc = time.time() - tic
 print("Data collection complete.",toc,"seconds elapsed.")
--- ReadAnalogVoltage/ReadAnalogVoltage.ino
+++ ReadAnalogVoltage/ReadAnalogVoltage.ino
@@ -1,27 +1,19 @@
-/*
-  ReadAnalogVoltage
-  Reads an analog input on pin 0, converts it to voltage, and prints the result to the serial monitor.
-  Graphical representation is available using serial plotter (Tools > Serial Plotter menu)
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
-
-  This example code is in the public domain.
-*/
-int x = 0;
-// the setup routine runs once when you press reset:
+// Reads analog voltage values and prints them to serial.
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
   // read the input on analog pin 0:
-  int sensorValue = analogRead(A0);
+  int loadCellVal = analogRead(A0);
+  int PTvalmin = analogRead(A7);
+  int PTvalmax = analogRead(A6);
   // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  float voltage = sensorValue * (5.0 / 1023.0);
+  float loadCellVoltage = loadCellVal * (5.0 / 1023.0);
+  float PTvoltage = (PTvalmax - PTvalmin) * (5.0 / 1023.0);
   // print out the value you read:
-  x = x + 1;
-  Serial.println(voltage);
-  //Serial.println(",");
-  //Serial.println(x);
+  Serial.print(loadCellVoltage);
+  Serial.print(",");
+  Serial.println(PTvoltage);
 }
