8bc8230f8e79221949bdc2117fefcf8204c32af9
mahoep <mhoeper3234@gmail.com>
2018-01-09 11:32:56 -0800

    updated baud rate

--- ReadAnalogVoltage/ReadAnalogVoltage.ino
+++ ReadAnalogVoltage/ReadAnalogVoltage.ino
@@ -1,7 +1,7 @@
 // Reads analog voltage values and prints them to serial.
 void setup() {
   // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
 
 void loop() {
@@ -9,9 +9,9 @@ void loop() {
   int loadCellVal = analogRead(A0);
   int PTval = analogRead(A7);
   // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  float loadCellVoltage = loadCellVal * (5.0 / 1023.0);
-  float PTvoltage = (PTval) * (5.0 / 1023.0);
-  float pressure_psi = (1132.08*(voltage) - 652.83);
+  double loadCellVoltage = loadCellVal * (5.0 / 1023.0);
+  double PTvoltage = (PTval) * (5.0 / 1023.0);
+  double pressure_psi = (1132.08*(PTvoltage) - 652.83);
   // print out the value you read:
   Serial.print(loadCellVoltage);
   Serial.print(",");
